33d55efe845fdf20ef7b62721073018fd4c3db11
Luis Canales <kopz9999@gmail.com>
2018-04-29 20:39:13 -0700

    setup AddToCart

--- ga-pixels.js
+++ ga-pixels.js
@@ -1,1 +1,13 @@
-console.log('hello 4');
+(function(jQuery){
+  function init() {
+    jQuery('[id*="addToCart"]').click(function () {
+      gtag('event', 'conversion', {
+        'event_category': 'AddToCart',
+      });
+    });
+  }
+
+  jQuery(document).ready(function(){
+    init();
+  });
+})(jQuery);
